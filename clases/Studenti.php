a0a56ff8b87eb6ec3230a04096d4ff920f38b4cc
smartSmark12 <kurfurst.vaclav12@gmail.com>
2025-12-10 09:21:57 +0100

    přidán výpis do studenti.php a opravena kontrola příjmení

--- clases/Studenti.php
+++ clases/Studenti.php
@@ -11,12 +11,14 @@ class Studenti {
     private $email;
     private $datum_registrace;
 
-    function nastavHodnoty($id, $jmeno, $datum_narozeni, $telefon, $email, $datum_registrace) {
+    function nastavHodnoty($id, $jmeno, $prijmeni, $datum_narozeni, $telefon, $email, $datum_registrace) {
 
         /* checkni hodnoty */ /* přidat ještě regex kontroly */
         if (gettype($id) != "integer" or is_null($id)) {
             return false;
         } else if (gettype($jmeno) != "string" or is_null($jmeno)) {
+            return false;
+        } else if (gettype($prijmeni) != "string" or is_null($prijmeni)) {
             return false;
         } else if (gettype($datum_narozeni) != "string" or is_null($datum_narozeni)) {
             return false;
@@ -38,11 +40,18 @@ class Studenti {
     }
 
     function vypis() {
-
+        /* vypíše echem data do article */
+        echo "<h3>ID: ".$this->$id;
+        echo "<p>Jméno: ".$this->$jmeno."</p>";
+        echo "<p>Příjmení: ".$this->$prijmeni."</p>";
+        echo "<p>Datum narození: ".$this->$datum_narozeni."</p>";
+        echo "<p>Telefon: ".$this->$telefon."</p>";
+        echo "<p>Email: ".$this->$email."</p>";
+        echo "<p>Datum registrace: ".$this->$datum_registrace."</p>";
     }
 
     function vypisOptions() {
-        
+
     }
 }
 
