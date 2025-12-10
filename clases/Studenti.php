5a7a7baf3f2f71fba3812f79b115818e24aad570
smartSmark12 <kurfurst.vaclav12@gmail.com>
2025-12-10 09:01:39 +0100

    přidání kontrol a nastavení hodnot do studenti.php

--- clases/Studenti.php
+++ clases/Studenti.php
@@ -14,9 +14,27 @@ class Studenti {
     function nastavHodnoty($id, $jmeno, $datum_narozeni, $telefon, $email, $datum_registrace) {
 
         /* checkni hodnoty */
+        if (gettype($id) != "integer" or is_null($id)) {
+            return false;
+        } else if (gettype($jmeno) != "string" or is_null($jmeno)) {
+            return false;
+        } else if (gettype($datum_narozeni) != "string" or is_null($datum_narozeni)) {
+            return false;
+        } else if (gettype($telefon) != "string" or is_null($telefon)) {
+            return false;
+        } else if (gettype($email) != "string" or is_null($email)) {
+            return false;
+        } else if (gettype($datum_registrace) != "string" or is_null($datum_registrace)) {
+            return false;
+        }
 
-        /* false pokud něco špatně */
-        return false;
+        /* nastav, pokud projdou všechny kontroly */
+        $this->$id = $id;
+        $this->$jmeno = $jmeno;
+        $this->$datum_narozeni = $datum_narozeni;
+        $this->$telefon = $telefon;
+        $this->$email = $email;
+        $this->$datum_registrace = $datum_registrace;
     }
 }
 
