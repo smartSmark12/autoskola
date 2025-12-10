e115af6853bbb3a577388d6e5400eef343bf8ccd
smartSmark12 <kurfurst.vaclav12@gmail.com>
2025-12-10 08:47:04 +0100

    úprava readme pro čitelnost a STUDENTI.PHP RAAAH

---
+++ clases/Studenti.php
@@ -0,0 +1,23 @@
+<!-- VK -->
+
+<?php
+
+class Studenti {
+    private $id;
+    private $jmeno;
+    private $prijmeni;
+    private $datum_narozeni;
+    private $telefon;
+    private $email;
+    private $datum_registrace;
+
+    function nastavHodnoty($id, $jmeno, $datum_narozeni, $telefon, $email, $datum_registrace) {
+
+        /* checkni hodnoty */
+
+        /* false pokud něco špatně */
+        return false;
+    }
+}
+
+?>
