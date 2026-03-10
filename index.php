c4381b78f3d7cf8fa9f857aabff4b3f28959ac8f
smartSmark12 <kurfurst.vaclav12@gmail.com>
2026-03-10 20:31:14 +0100

    base desinng ✨

--- index.php
+++ index.php
@@ -6,32 +6,41 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Autoškola main</title>
+    <link rel="stylesheet" href="bordel/style.css">
 </head>
 <body>
     <h1>Autoškola první řady</h1>
-    <div>
-        <h2>vložení záznamů</h2>
+    <main>
         <div>
-            <a href="/forms/form-auta.php">Auta vložení</a>
-            <a href="/forms/form-instruktori.php">Instruktoři vložení</a>
-            <a href="/forms/form-studenti.php">Studenti vložení</a>
+            <h2>Vložení záznamů</h2>
+            <div class="form-list">
+                <a href="forms/form-auta.php">Auta vložení</a>
+                <a href="forms/form-instruktori.php">Instruktoři vložení</a>
+                <a href="forms/form-studenti.php">Studenti vložení</a>
+            </div>
         </div>
-        <h2>odebírání záznamů</h2>
         <div>
-            <a href="/forms_remove/form-auta.php">Auta odebrání</a>
-            <a href="/forms_remove/form-instruktori.php">Instruktoři odebrání</a>
-            <a href="/forms_remove/form-studenti.php">Studenti odebrání</a>
+            <h2>Odebírání záznamů</h2>
+            <div class="form-list">
+                <a href="forms_remove/form-auta.php">Auta odebrání</a>
+                <a href="forms_remove/form-instruktori.php">Instruktoři odebrání</a>
+                <a href="forms_remove/form-studenti.php">Studenti odebrání</a>
+            </div>
         </div>
-        <h2>výpis záznamů</h2>
         <div>
-            <a href="/forms/form-auta.php">Auta výpis</a>
-            <a href="/forms/form-instruktori.php">Instruktoři výpis</a>
-            <a href="/forms/form-studenti.php">Studenti výpis</a>
+            <h2>Výpis záznamů</h2>
+            <div class="form-list">
+                <a href="forms_display/form-auta.php">Auta výpis</a>
+                <a href="forms_display/form-instruktori.php">Instruktoři výpis</a>
+                <a href="forms_display/form-studenti.php">Studenti výpis</a>
+            </div>
         </div>
-        <h2>výpis administrativa</h2>
         <div>
-            <a href="/form_admin/form-administrativa.php">Administrativa výpis</a>
+            <h2>Výpis administrativa</h2>
+            <div class="form-list">
+                <a href="form_admin/form-administrativa.php">Administrativa výpis</a>
+            </div>
         </div>
-    </div>
+    </main>
 </body>
 </html>
